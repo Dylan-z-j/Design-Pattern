008d9789f76ae516896102265dfd468e8ca346e1
Jingzhe_zhu <79922413+Dylan-z-j@users.noreply.github.com>
2022-01-09 13:51:53 +0800

    Update demo.py

--- 3. 单例模式/demo.py
+++ 3. 单例模式/demo.py
@@ -1,20 +1,25 @@
 import threading
 import time
+
 #这里使用方法__new__来实现单例模式
+
 class Singleton(object):#抽象单例
     def __new__(cls, *args, **kw):
         if not hasattr(cls, '_instance'):
             orig = super(Singleton, cls)
             cls._instance = orig.__new__(cls, *args, **kw)
         return cls._instance
+    
 #总线
+
 class Bus(Singleton):
     lock = threading.RLock()
     def sendData(self,data):
         self.lock.acquire()
         time.sleep(3)
-        print "Sending Signal Data...",data
+        print("Sending Signal Data...", data)
         self.lock.release()
+        
 #线程对象，为更加说明单例的含义，这里将Bus对象实例化写在了run里
 class VisitEntity(threading.Thread):
     my_bus=""
@@ -29,7 +34,7 @@ class VisitEntity(threading.Thread):
 
 if  __name__=="__main__":
     for i in range(3):
-        print "Entity %d begin to run..."%i
+        print("Entity %d begin to run..." % i)
         my_entity=VisitEntity()
         my_entity.setName("Entity_"+str(i))
         my_entity.start()
